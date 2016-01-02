f3c1824a41e1d78619b9b583fa5692d78a0f7d03
Marc Ihm <marc.ihm@schufa.de>
2016-01-02 21:31:00 +0100

    Import to github, version 5.0 of org-index

---
+++ screencast.bat
@@ -0,0 +1,6 @@
+set LANG="de_DE.UTF-8"
+set ORG=C:\org
+set HOME=C:
+cd %ORG%
+
+C:\emacs\emacs-24.5\bin\runemacs.exe -debug-init -g 82x36 -T "Introduction to org-index" -fn "-outline-Lucida Console-normal-normal-normal-mono-17-*-*-*-c-*-iso8859-1" -l C:/org/index/screencast-init.el -l C:/org/index/screencast.el C:/org/index/screencast-init.el C:/org/index/screencast.el c:/org/index/oidx.org c:/org/index/screencast.org c:/org/index/screencast.bat c:/org/index/demo.org c:/org/index/org-index.el 
---
+++ test.bat
@@ -0,0 +1,6 @@
+set LANG="de_DE.UTF-8"
+set ORG=C:\org
+set HOME=C:
+cd %ORG%
+
+C:\emacs\emacs-24.5\bin\runemacs.exe -debug-init -fn "-outline-Lucida Console-normal-r-normal-normal-18-97-96-96-c-*-iso8859-1" -l C:/org/index/test-init.el -l C:/org/index/oidx.el C:/org/index/org-index.el c:/org/index/oidx.org c:/org/index/oidx.el
