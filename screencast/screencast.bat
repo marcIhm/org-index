2b38e35ba5330a1e707f9207e73a196407c06e2d
Marc Ihm <marc.ihm@schufa.de>
2018-08-12 14:29:39 +0200

    doing screencast under unix

--- screencast/screencast.bat
+++
@@ -1,6 +0,0 @@
-set LANG="de_DE.UTF-8"
-set ORG=C:\org
-set HOME=C:
-cd %ORG%
-
-C:\emacs\emacs-25.1\bin\runemacs.exe -debug-init -g 82x36 -T "Introduction to org-index" -fn "-outline-Lucida Console-normal-normal-normal-mono-17-*-*-*-c-*-iso8859-1" -l C:/org/org-index/screencast-init.el -l C:/org/org-index/screencast.el C:/org/org-index/screencast-init.el C:/org/org-index/screencast.el c:/org/org-index/oidx.org c:/org/org-index/screencast.org c:/org/org-index/screencast.bat c:/org/org-index/demo.org c:/org/org-index/org-index.el 
---
+++ screencast/screencast.sh
@@ -0,0 +1,1 @@
+emacs -debug-init -g 82x36 -T "Introduction to org-index" -l screencast-init.el -l screencast.el screencast-init.el screencast.el screencast.org screencast.sh ../org-index.el 
