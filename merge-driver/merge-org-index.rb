905e3dba6bdc009aa3fe7df0391f5f46afc0f68d
Marc Ihm <marc.ihm@schufa.de>
2018-11-12 20:59:28 +0100

    better error handling

--- merge-driver/merge-org-index.rb
+++ merge-driver/merge-org-index.rb
@@ -104,7 +104,7 @@ class OrgTable < Hash
     self[:table_caption].split(/\R/)[0].split("|").drop(1).map(&:strip).map {|s| s.sub(/-/,"_")}.map(&:to_sym).each_with_index {|x,i| columns[x] = i}
     self[:table_columns] = columns
     self[:has_marker_lines] = false
-    
+
     lines = Hash.new
     self[:table_body].split(/\R/).each_with_index do |line,lineno|
       next if line =~ /^\s*$/
@@ -116,7 +116,7 @@ class OrgTable < Hash
         self[:has_marker_lines] = true
         next
       end
-      uid = [:ref, :id, :yank].map { |c| cols[columns[c]].strip }.join("|||")
+      uid = [:ref, :id, :yank].map { |c| cols[columns[c]].strip }.join("~")
       if lines[uid]
         warn "Line with uid '#{uid}' appears again: #{line}"
         self[:duplicates] ||= ""
@@ -139,28 +139,23 @@ class OrgTable < Hash
     self[:lines][key][:line].gsub(/\s+\|/," |").gsub(/\|\s+/,"| ")
   end
 
-  def format_line fields
+  def make_description_line( description, markup )
     fmttd = self[:table_hline].dup
     fmttd.gsub!('+','|')
     self[:table_columns].each_key do |key|
-      if fields[key]
-        text = fields[key]
-        markup = ' '
-      else
-        text = ''
-        markup = fields[:markup] || ' '
-      end
+      (text,fill) = (key == :keywords) ? [description, ' '] : ['', markup]
       fmttd.sub!(/\|-+\|/) do |match|
-        ('| ' + text + markup * match.length)[0,match.length - 2] + ' |'
+        ('| ' + text + fill * match.length)[0,match.length - 2] + ' |'
       end
+      fmttd
     end
     fmttd.chomp + "\n"
   end
   
-  def format part,text
+  def format_part( part, text )
     fmttd = ""
     if part.length > 0
-      fmttd = self.format_line(:keywords => text, :markup => 'w') if text
+      fmttd = self.make_description_line( text, 'w' ) if text
       part.each { |key| fmttd += self[:lines][key][:line]; }
     end
     fmttd
@@ -168,7 +163,7 @@ class OrgTable < Hash
          
   def format_duplicates text
     if self[:duplicates]
-      self.format_line(:keywords => text, :markup => 'w') + self[:duplicates].chomp + "\n"
+      self.make_description_line( text, 'w' ) + self[:duplicates].chomp + "\n"
     else
       ""
     end
@@ -187,6 +182,10 @@ edit_hint = "  This editor is invoked to let you merge changes in your index;
     + : Should be kept
     - : Should be removed
 
+  PLEASE NOTE, that 'current' and 'other' are to be viewed from
+  the perspective of the master-repository; so your local 
+  changes are marked with 'other'.
+
   Description for some sections:
     duplicates: lines, that also appear below in 'common' and can
       be removed.
@@ -200,22 +199,33 @@ edit_hint = "  This editor is invoked to let you merge changes in your index;
   Having edited all the sections you may continue like this:
   (assuming that you started with 'git rebase'):
   - 'git status' to see if any other file needs merging
-  - edit those other files manually with an editor
+  - edit those other files (shown in red) manually with an editor
   - 'git add' for all files that you edited, especially your index
   - 'git rebase --continue' to finish the merge
+  - The whole cycle may repeat a few times
 
 "
 
-# Remark, the sequence of arguments (%O, %A and %B) is determined in .git/config
+# Remark, the lines below (handling ARGV) assume, that .git/config specifies the arguments to this
+# merge-driver as "%O %A %B" where %O = ancestor, %A = current and %B = other
 ancestor = OrgTable.new(ARGV[0],"ancestor")
 other = OrgTable.new(ARGV[1],"other")
 current = OrgTable.new(ARGV[2],"current")
+edited_name = ARGV[2]
+destination = ARGV[4]
 
 [:before_heading, :heading, :before_table, :table_caption, :table_hline, :after_table].each do |part|
-  warn "Part #{part.to_s} does not match in current and other" unless current[part] == other[part]
+  unless current[part] == other[part]
+    if part == :table_caption && current[:table_caption].lines[0] != other[:table_caption].lines[0]
+      fail "\n\nPart table_caption does not match in current and other; please consider, fixing this manually !\n\n#{current[:table_caption].lines[0]}\n#{other[:table_caption].lines[0]}\n\n"
+    else
+      warn "Part #{part.to_s} does not match in current and other."
+    end
+  end
 end
 
 puts edit_hint
+puts "Editing #{destination} ..."
 
 File.open(ARGV[1],'w') do |file|
 
@@ -235,67 +245,67 @@ File.open(ARGV[1],'w') do |file|
 
   inters = current[:lines].keys & other[:lines].keys
 
-  cadd = Array.new
-  ormv = Array.new
+  current_added = Array.new
+  other_removed = Array.new
   (current[:lines].keys - inters).each do |key|
     if ancestor[:lines][key]
-      ormv << key
+      other_removed << key
     else
-      cadd << key
+      current_added << key
     end
   end
 
-  oadd = Array.new
-  crmv = Array.new
+  other_added = Array.new
+  current_removed = Array.new
   (other[:lines].keys - inters).each do |key|
     if ancestor[:lines][key]
-      crmv << key
+      current_removed << key
     else
-      oadd << key
+      other_added << key
     end
   end
   
-  cmod = Array.new
-  omod = Array.new
+  current_modified = Array.new
+  other_modified = Array.new
   common = Array.new
   conflicts = Array.new
   inters.each do |key|
     if current.digest(key) == other.digest(key)
       common << key
     elsif other.digest(key) == ancestor.digest(key)
-      cmod << key
+      current_modified << key
     elsif current.digest(key) == ancestor.digest(key)
-      omod << key
+      other_modified << key
     else
       conflicts << key
     end
   end
 
-  file.write current.format(conflicts,"? CONFLICTS, current") +
-             other.format(conflicts,"? CONFLICTS, other") +
-             ancestor.format(conflicts,"? CONFLICTS, ancestor") +
-             current.format(cadd,"+ current added") +
-             ancestor.format(crmv,"- current removed") +
-             other.format(oadd,"+ other added") +
-             ancestor.format(ormv,"- other removed") +
-             current.format_duplicates("- current duplicates") +
-             other.format_duplicates("- other duplicates") +
-             current.format(cmod,"+ current modified") +
-             other.format(omod,"+ other modified") +
-             current.format(common,"+ common") +
+  file.write current.format_part( conflicts, "? CONFLICTS, current" ) +
+             other.format_part( conflicts, "? CONFLICTS, other" ) +
+             ancestor.format_part( conflicts, "? CONFLICTS, ancestor" ) +
+             current.format_part( current_added, "+ current added" ) +
+             ancestor.format_part( current_removed, "- current removed" ) +
+             other.format_part( other_added, "+ other added" ) +
+             ancestor.format_part( other_removed, "- other removed" ) +
+             current.format_duplicates( "- current duplicates" ) +
+             other.format_duplicates( "- other duplicates" ) +
+             current.format_part( current_modified, "+ current modified" ) +
+             other.format_part( other_modified, "+ other modified" ) +
+             current.format_part( common, "+ common" ) +
              current[:after_table]
 
 end
 
 file = Dir.pwd + "/" + ARGV[1]
-args = ["(setq org-startup-folded 2)",
-        "(setq org-startup-align-all-tables t)",
-        "(pop-to-buffer (find-file \"#{file}\"))",
-        "(org-mode)"].map {|x| ["--eval",x]}.flatten
+args = [ "(setq org-startup-folded 2)",
+         "(setq org-startup-align-all-tables t)",
+         "(pop-to-buffer (find-file \"#{file}\"))",
+         "(org-mode)" ].map {|x| ["--eval",x]}.flatten
 puts "Executing: " + options[:emacs] + " " + args.join(" ")
 system options[:emacs], *args
 puts "Trying to parse the edited file ..."
-edited=OrgTable.new(ARGV[1],"edited")
+edited = OrgTable.new(edited_name,"edited")
 warn "Edit hint has not been removed from #{ARGV[1]}" if edited[:before_table].include?("wwwww")
 fail "Found marker lines in #{ARGV[1]}" if edited[:has_marker_lines]
 puts "Done."
