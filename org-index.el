1f029b28b817dcdc7d1deb734a087ba08e66c6b5
Marc Ihm <marc.ihm@schufa.de>
2021-02-26 17:35:04 +0100

    chrome

--- org-index.el
+++ org-index.el
@@ -13,7 +13,6 @@
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
-
 ;; the Free Software Foundation; either version 3, or (at your option)
 ;; any later version.
 ;;
