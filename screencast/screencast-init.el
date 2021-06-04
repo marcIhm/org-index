40716a4cfd36304b4a16977020b1e82870faec15
Marc Ihm <marc.ihm@schufa.de>
2021-06-04 08:06:31 +0200

    chrome

--- screencast/screencast-init.el
+++ screencast/screencast-init.el
@@ -29,12 +29,45 @@
  ;; If there is more than one, they won't work right.
  '(c-basic-offset 4)
  '(diff-switches "-u")
+ '(make-backup-files nil)
  '(menu-bar-mode nil)
- '(org-index-id "ef6e9f2a-e6c0-4f28-a476-7d40e16a61a5")
- '(org-index-key "i"))
+ '(org-index-id "1667e430-d529-4f24-8cdf-74e5c4ef7b5f")
+ '(org-index-key "i")
+ '(package-selected-packages '(dash s org-id-cleanup ivy helm))
+ '(safe-local-variable-values
+   '((eval when
+           (and
+            (buffer-file-name)
+            (not
+             (file-directory-p
+              (buffer-file-name)))
+            (string-match-p "^[^.]"
+                            (buffer-file-name)))
+           (unless
+               (featurep 'package-build)
+             (let
+                 ((load-path
+                   (cons "../package-build" load-path)))
+               (require 'package-build)))
+           (unless
+               (derived-mode-p 'emacs-lisp-mode)
+             (emacs-lisp-mode))
+           (package-build-minor-mode)
+           (setq-local flycheck-checkers nil)
+           (set
+            (make-local-variable 'package-build-working-dir)
+            (expand-file-name "../working/"))
+           (set
+            (make-local-variable 'package-build-archive-dir)
+            (expand-file-name "../packages/"))
+           (set
+            (make-local-variable 'package-build-recipes-dir)
+            default-directory))))
+ '(vc-follow-symlinks t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(isearch ((t (:background "green" :foreground "cyan1"))))
+ '(mode-line ((t (:background "black" :foreground "green" :inverse-video t :weight extra-light)))))
